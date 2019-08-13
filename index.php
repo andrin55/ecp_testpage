f15502c590edde7b734f7ba66e08ed79d1931de4
andrin55 <andrin55@users.noreply.github.com>
2019-08-13 14:11:00 +0200

    Update index.php

--- index.php
+++ index.php
@@ -4,6 +4,7 @@ header("Cache-Control: no-store, no-cache, must-revalidate, max-age=0");
 header("Cache-Control: post-check=0, pre-check=0", false);
 header("Pragma: no-cache");
 echo "<b>PoC Enterprise Container Platform Andrin Brunner</b>";
+echo "<b>test</b>";
 echo "<br><br>";
 date_default_timezone_set("Europe/Zurich");
 echo "Current container time: " . date("Y.m.d H:i:s");
